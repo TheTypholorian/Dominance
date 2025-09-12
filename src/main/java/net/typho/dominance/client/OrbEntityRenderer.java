359ba7b91a29ff94abad9fe028ccac814a685835
The Typhothanian <thetyphothanian@gmail.com>
2025-09-11 20:35:00 -0600

    orb of dominance entity working

--- src/main/java/net/typho/dominance/client/OrbEntityRenderer.java
+++ src/main/java/net/typho/dominance/client/OrbEntityRenderer.java
@@ -2,15 +2,19 @@ package net.typho.dominance.client;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import foundry.veil.api.client.render.rendertype.VeilRenderType;
-import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.render.VertexConsumer;
-import net.minecraft.client.render.VertexConsumerProvider;
+import foundry.veil.api.client.render.vertex.VertexArray;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.*;
 import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Vec3d;
 import net.typho.dominance.Dominance;
 import net.typho.dominance.OrbEntity;
+import org.joml.Matrix4fStack;
+import org.joml.Quaternionf;
+import org.lwjgl.glfw.GLFW;
 
 public class OrbEntityRenderer extends EntityRenderer<OrbEntity> {
     protected OrbEntityRenderer(EntityRendererFactory.Context ctx) {
@@ -19,63 +23,71 @@ public class OrbEntityRenderer extends EntityRenderer<OrbEntity> {
 
     @Override
     public void render(OrbEntity entity, float yaw, float tickDelta, MatrixStack stack, VertexConsumerProvider vertexConsumers, int light) {
-        try {
-            RenderLayer layer = VeilRenderType.get(Dominance.id("orb"));
+        Vec3d cam = MinecraftClient.getInstance().gameRenderer.getCamera().getPos();
+        RenderLayer layer = VeilRenderType.get(Dominance.id("orb"));
 
-            if (layer != null) {
-                VertexConsumer consumer = vertexConsumers.getBuffer(layer);
-                RenderSystem.enableDepthTest();
+        if (layer != null) {
+            BufferBuilder builder = RenderSystem.renderThreadTesselator().begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION);
 
-                stack.push();
-
-                for (int k = 0; k < 1; k++) {
-                    if (k == 0) {
-                        consumer.vertex(0f, 0f, 1f);
-                        consumer.vertex(0f, 1f, 1f);
-                        consumer.vertex(1f, 1f, 1f);
-                        consumer.vertex(1f, 0f, 1f);
-                    }
-
-                    if (k == 1) {
-                        consumer.vertex(1f, 0f, 1f);
-                        consumer.vertex(1f, 1f, 1f);
-                        consumer.vertex(1f, 1f, 0f);
-                        consumer.vertex(1f, 0f, 0f);
-                    }
-
-                    if (k == 2) {
-                        consumer.vertex(1f, 0f, 0f);
-                        consumer.vertex(1f, 1f, 0f);
-                        consumer.vertex(0f, 1f, 0f);
-                        consumer.vertex(0f, 0f, 0f);
-                    }
-
-                    if (k == 3) {
-                        consumer.vertex(0f, 0f, 0f);
-                        consumer.vertex(0f, 1f, 0f);
-                        consumer.vertex(0f, 1f, 1f);
-                        consumer.vertex(0f, 0f, 1f);
-                    }
-
-                    if (k == 4) {
-                        consumer.vertex(0f, 0f, 0f);
-                        consumer.vertex(0f, 0f, 1f);
-                        consumer.vertex(1f, 0f, 1f);
-                        consumer.vertex(1f, 0f, 0f);
-                    }
-
-                    if (k == 5) {
-                        consumer.vertex(0f, 1f, 1f);
-                        consumer.vertex(0f, 1f, 0f);
-                        consumer.vertex(1f, 1f, 0f);
-                        consumer.vertex(1f, 1f, 1f);
-                    }
+            for (int k = 0; k < 6; k++) {
+                if (k == 0) {
+                    builder.vertex(0f, 0f, 1f);
+                    builder.vertex(0f, 1f, 1f);
+                    builder.vertex(1f, 1f, 1f);
+                    builder.vertex(1f, 0f, 1f);
                 }
 
-                stack.pop();
+                if (k == 1) {
+                    builder.vertex(1f, 0f, 1f);
+                    builder.vertex(1f, 1f, 1f);
+                    builder.vertex(1f, 1f, 0f);
+                    builder.vertex(1f, 0f, 0f);
+                }
+
+                if (k == 2) {
+                    builder.vertex(1f, 0f, 0f);
+                    builder.vertex(1f, 1f, 0f);
+                    builder.vertex(0f, 1f, 0f);
+                    builder.vertex(0f, 0f, 0f);
+                }
+
+                if (k == 3) {
+                    builder.vertex(0f, 0f, 0f);
+                    builder.vertex(0f, 1f, 0f);
+                    builder.vertex(0f, 1f, 1f);
+                    builder.vertex(0f, 0f, 1f);
+                }
+
+                if (k == 4) {
+                    builder.vertex(0f, 0f, 0f);
+                    builder.vertex(0f, 0f, 1f);
+                    builder.vertex(1f, 0f, 1f);
+                    builder.vertex(1f, 0f, 0f);
+                }
+
+                if (k == 5) {
+                    builder.vertex(0f, 1f, 1f);
+                    builder.vertex(0f, 1f, 0f);
+                    builder.vertex(1f, 1f, 0f);
+                    builder.vertex(1f, 1f, 1f);
+                }
             }
-        } catch (Throwable t) {
-            t.printStackTrace();
+
+            try (VertexArray array = VertexArray.create()) {
+                array.upload(builder.end(), VertexArray.DrawUsage.STATIC);
+
+                Matrix4fStack stack1 = RenderSystem.getModelViewStack();
+                stack1.pushMatrix();
+                stack1.translate((float) (entity.getPos().getX() - cam.x - 0.5), (float) (entity.getPos().getY() - cam.y), (float) (entity.getPos().getZ() - cam.z - 0.5));
+                stack1.rotateAround(new Quaternionf().rotationXYZ((float) GLFW.glfwGetTime(), 0, (float) GLFW.glfwGetTime()), 0.5f, 0.5f, 0.5f);
+                RenderSystem.applyModelViewMatrix();
+
+                array.bind();
+                array.drawWithRenderType(layer);
+
+                stack1.popMatrix();
+                RenderSystem.applyModelViewMatrix();
+            }
         }
 
         super.render(entity, yaw, tickDelta, stack, vertexConsumers, light);
--- src/main/resources/assets/dominance/pinwheel/shaders/program/orb.fsh
+++ src/main/resources/assets/dominance/pinwheel/shaders/program/orb.fsh
@@ -1,11 +1,16 @@
 uniform sampler2D DiffuseSampler0;
-uniform sampler2D DiffuseDepthSampler;
-
-uniform float uTime = 0.5;
+uniform float GameTime;
+uniform vec2 ScreenSize;
+uniform mat4 ProjMat;
 
 out vec4 fragColor;
 
+float linearDepth(float depth, mat4 proj) {
+    return proj[3][2] / (depth * 2 - 1 + proj[2][2]);
+}
+
 void main() {
-    fragColor = vec4(mod(uTime, 1), 0, 0, 1);
-    //fragColor = vec4(1, 0.5, 0.25, 1);
+    vec4 min = vec4(1, 0.41, 1, 1);
+    vec4 max = vec4(1, 0.76, 0.4, 1);
+    fragColor = ((max - min) * (sin(GameTime * 3000) / 2 + 0.5) + min) * sin(linearDepth(gl_FragCoord.z, ProjMat) + GameTime * 3000) / 4 + 1.25;
 }
